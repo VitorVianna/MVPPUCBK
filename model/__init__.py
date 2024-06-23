1fcb131ef4c4851b2fe2e2462af3146c682c79c2
Vitor Henrique Vianna Reis <tetrisit.vreis@terceiros.mag.com.br>
2024-06-23 12:52:03 -0300

    update Requirements & fix bug on Database folder

--- model/__init__.py
+++ model/__init__.py
@@ -1,7 +1,7 @@
 from sqlalchemy_utils import database_exists, create_database
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
-
+import os
 # importando os elementos definidos no modelo
 from model.base import Base
 from model.cliente import Cliente
@@ -9,9 +9,14 @@ from model.profissional import Profissional
 from model.agenda import Agenda
 
 
-
+db_path = "database/"
+# Verifica se o diretorio não existe
+if not os.path.exists(db_path):
+   # então cria o diretorio
+   os.makedirs(db_path)
 # url de acesso ao banco (essa é uma url de acesso ao sqlite local)
-db_url = 'sqlite:///database/db.sqlite3'
+db_url = 'sqlite:///%s/db.sqlite3' % db_path
+
 
 # cria a engine de conexão com o banco
 engine = create_engine(db_url, echo=False)
